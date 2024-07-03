4af5703cabbfe4034d5783ac5597e8160237469e
sayedalali1 <sayedalali1@gmail.com>
2024-07-03 10:46:43 +0300

    intial commit

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // empty file, no JS needed.
+// querySelector(selectors)
+const [sun, moon] = document.querySelectorAll("i");
+moon.addEventListener("click", ()=>{
+    document.querySelector("body").className="dark"
+})
+sun.addEventListener("click", ()=>{
+    document.querySelector("body").className=""
+})
+
+moon.addEventListener("mouseenter", ()=>{
+    moon.style.borderBottom = "3px solid"
+})
+moon.addEventListener("mouseout", ()=>{
+    moon.style.borderBottom = ""
+})
+sun.addEventListener("mouseenter", ()=>{
+    sun.style.borderBottom = "3px solid"
+})
+sun.addEventListener("mouseout", ()=>{
+    sun.style.borderBottom = ""
+})
+
+
+// addEventListener("mouseover", (event) => {});
+
+// onmouseover = (event) => {};
